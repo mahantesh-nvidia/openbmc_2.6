111ee612f42243fdd27f0a7d823b4859bf9889a2
Diane Lynch <dianel@mellanox.com>
2019-02-25 13:30:48 -0500

    Update BMC fan controller state machine.
    Add read of NIC I2C to get BF OCP temp.
    Set fan RPM to higher setting of board vs Bluefield cooling profile.
    
    RM #1616716
    
    Change-Id: I19f63b81d4551f798e86a63d473a2fb735845aa7

--- meta-mellanox/meta-bluewhale-ast2500/recipes-phosphor/fans/files/obmc-mellanox-fand.sh
+++ meta-mellanox/meta-bluewhale-ast2500/recipes-phosphor/fans/files/obmc-mellanox-fand.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Version 0.1
+# Version 0.2
 
 # This script implements a basic temperature-based fan-control algorithm
 
@@ -21,28 +21,46 @@ do
     fi
 done
 
-# The algorithm decides the fan speed based on the following temp ranges:
+# Check BlueField internal temp via NIC I2C
+bf_temp_raw=$(/usr/sbin/i2cget -y 5 0x1f 1)
+m=1000
+bf_temp=$(($bf_temp_raw * $m))
+
+# Fan speed is based on the following board sensor temp ranges:
 # 0 (error case) : 100% duty cycle (max speed - default setting)
-# 1 to 14999     : 25% duty cycle
+# less than 14999: 25% duty cycle
 # 15000 to 29999 : 50% duty cycle
 # 30000 to 44999 : 75% duty cycle
-# Above 45000    : 100% dudty cycle (max speed)
-# Note that temperature readings are inetrpreted as (value/1000) deg C
+# Above 45000    : 100% duty cycle (max speed)
+# Note that temperature readings are interpreted as (value/1000) deg C
 # i.e. A temperature reading of 15000 corresponds to 15 deg C
 temp_limit1=15000
 temp_limit2=30000
 temp_limit3=45000
+
+# And these are the Bluefield SOC temp ranges:
+# 0 (error case) : 100% duty cycle (max speed - default setting)
+# less than 39999: 25% duty cycle
+# 40000 to 59999 : 50% duty cycle
+# 60000 to 74999 : 75% duty cycle
+# Above 75000    : 100% duty cycle (max speed)
+bf_temp_limit1=40000
+bf_temp_limit2=60000
+bf_temp_limit3=75000
+
 duty_cycle_25=63
 duty_cycle_50=127
 duty_cycle_75=191
 duty_cycle_100=255
-if [ $max -eq 0 ]; then
+
+# Set fans to the higher speed, as per board and BlueField temp profiles
+if [[ $max -eq 0 ]] || [[ $bf_temp -eq 0 ]] ; then
     pwm=$duty_cycle_100
-elif [ $max -lt $temp_limit1 ]; then
+elif [[ $max -lt $temp_limit1 ]] && [[ $bf_temp -lt $bf_temp_limit1 ]] ; then
     pwm=$duty_cycle_25
-elif [ $max -lt $temp_limit2 ]; then
+elif [[ $max -lt $temp_limit2 ]] && [[ $bf_temp -lt $bf_temp_limit2 ]] ; then
     pwm=$duty_cycle_50
-elif [ $max -lt $temp_limit3 ]; then
+elif [[ $max -lt $temp_limit3 ]] && [[ $bf_temp -lt $bf_temp_limit3 ]] ; then
     pwm=$duty_cycle_75
 else
     pwm=$duty_cycle_100
@@ -60,9 +78,9 @@ change_fan_speed() {
 }
 
 if [ $pwm -gt $pwm_curr ]; then
-    echo "Mellanox Fan Controller: Increasing Fan Speed"
+    echo "Mellanox Fan Controller: Increasing Fan Speed:$pwm. Max Board Temp: $max BF Temp: $bf_temp"
     change_fan_speed
 elif [ $pwm -lt $pwm_curr ]; then
-    echo "Mellanox Fan Controller: Decreasing Fan Speed"
+    echo "Mellanox Fan Controller: Decreasing Fan Speed:$pwm. Max Board Temp: $max BF Temp: $bf_temp"
     change_fan_speed
 fi
