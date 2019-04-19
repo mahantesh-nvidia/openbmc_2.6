f37fa7dfd8a72acdc89dcccf860ea6dd49c50e03
Asmaa Mnebhi <Asmaa@mellanox.com>
2019-04-19 11:18:41 -0400

    Cleanup the ipmbd script
    1) replace logic in the ipmbd script with mlnx_powerstatus_bf script
    2) Add logic to check whether the BF side IPMB driver is loaded.
    
    This is a follow up to this change: 1749412
    
    Issue: 1749792
    RM #1749792
    
    Change-Id: Id5b361e98bae41c2545d75197eb0fc174beab5d6

--- meta-mellanox/meta-bluewhale-ast2500/recipes-connectivity/ipmb-driver/files/obmc-mellanox-ipmbd.sh
+++ meta-mellanox/meta-bluewhale-ast2500/recipes-connectivity/ipmb-driver/files/obmc-mellanox-ipmbd.sh
@@ -1,34 +1,23 @@
 #!/bin/sh
 
-# Date modified: 02/20/2019
+# Date modified: 04/19/2019
 
 # This script checks if the IPMB driver is not yet loaded and
 # if the BF is up via the BMC GPIO pins 218.
 # If the check is true, then we try to load the IPMB module.
 
 if ! lsmod | grep ipmb_host &> /dev/null; then
+	mlnx_powerstatus_bf
 
-	# From Aspeed GPIO driver sysfs, get base number for GPIO access
-	GPIO_BASE=$(cat /sys/devices/platform/ahb/ahb:apb/1e780000.gpio/gpio/*/base)
-
-	# Use base number to calculate GPIO numbers for pins 218
-	GPIO_NUM_218=$(($GPIO_BASE + 218))
-
-	# If any other script is using GPIO pin 218, exit this script,
-	# and try again later.
-	if [ -d /sys/class/gpio/gpio${GPIO_NUM_218} ]; then
-		exit
-	fi
-
-	echo ${GPIO_NUM_218} > /sys/class/gpio/export
-
-	# read current gpio pin values
-	currval218=$(cat /sys/class/gpio/gpio${GPIO_NUM_218}/value)
-
-	if [ $currval218 -eq 1 ]; then
+	if [ $? -eq 1 ]; then
 		if modinfo ipmb_host &> /dev/null; then
 			modprobe ipmb_host
+			last_msg=$(dmesg | grep ipmb-host | tail -1 | cut -f 2 -d ":")
+			if [ $last_msg == " probe of 12-0020 failed with error -5" ]; then
+				# This means that although the BF is up, it has not
+				# yet loaded the IPMB driver. So, try again later.
+				rmmod ipmb_host
+			fi
 		fi
 	fi
-	echo ${GPIO_NUM_218} > /sys/class/gpio/unexport
 fi
