748baf135e15869451ec9fe29530006a240a8465
Asmaa Mnebhi <Asmaa@mellanox.com>
2019-09-06 15:25:11 -0400

    Change BF ipmb address to 0x11
    The BF Address 0x12 has some minor issue similar to when using address
    0x30. So for now, we will make it 0x11 until we are able to debug this
    further. I also took this chance to fix a bug in the
    obmc-mellanox.ipmbd.sh script where the failure string is never
    taken into account. It was just missing the quotes around the variable.
    
    Change-Id: I6e3110e5f6158453591b3a67307bc5bcf03933a6

--- meta-mellanox/meta-bluewhale-ast2500/recipes-connectivity/ipmb-driver/files/obmc-mellanox-ipmbd.sh
+++ meta-mellanox/meta-bluewhale-ast2500/recipes-connectivity/ipmb-driver/files/obmc-mellanox-ipmbd.sh
@@ -6,8 +6,9 @@
 # if the BF is up via the BMC GPIO pins 218.
 # If the check is true, then we try to load the IPMB module.
 I2C12_DEV_PATH=/sys/bus/i2c/devices/i2c-12/new_device
+I2C12_DEL_DEV_PATH=/sys/bus/i2c/devices/i2c-12/delete_device
 IPMB_HOST_ADD=0x1020
-SLAVE_ADD=0x12
+SLAVE_ADD=0x11
 
 if ! lsmod | grep ipmb_host &> /dev/null; then
 	mlnx_powerstatus_bf
@@ -15,14 +16,12 @@ if ! lsmod | grep ipmb_host &> /dev/null; then
 	if [ $? -eq 1 ]; then
 		if modinfo ipmb_host &> /dev/null; then
 			modprobe ipmb_host slave_add=$SLAVE_ADD
-			last_msg=$(dmesg | grep ipmb-host | tail -1 | cut -f 2 -d ":")
-			if [ $last_msg == " probe of 12-0020 failed with error -5" ]; then
-				# This means that although the BF is up, it has not
-				# yet loaded the IPMB driver. So, try again later.
+			echo ipmb-host $IPMB_HOST_ADD > $I2C12_DEV_PATH
+			if [ ! -e "/dev/ipmi0" ]; then
 				rmmod ipmb_host
+				echo $IPMB_HOST_ADD > $I2C12_DEL_DEV_PATH
 				exit 1
 			fi
-			echo ipmb-host $IPMB_HOST_ADD > $I2C12_DEV_PATH
 		fi
 	fi
 fi
